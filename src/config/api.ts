fca6d3a2ff3873819cd2cd93437517e94f698abe
a3onny <a3onny@gmail.com>
2021-05-03 20:02:10 +0800

    build: 更改CLI配置

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,9 @@
-// import { isDev } from '@/utils/is';
+import { isDev } from '@/utils/is';
 import { AxiosRequestConfig } from 'axios';
 
 const config: AxiosRequestConfig = {
   timeout: 1000 * 60,
-  // baseURL: isDev ? process.env.VUE_APP_API_DEV : '',
-  baseURL: '/api',
+  baseURL: isDev ? process.env.VUE_APP_API_DEV : process.env.VUE_APP_API_PROD,
   withCredentials: true,
 };
 
--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const TerserPlugin = require('terser-webpack-plugin');
 const zlib = require('zlib');
-
-const isDev = process.env.NODE_ENV === 'development';
 
 /**
  * @type import('@vue/cli-service/types/ProjectOptions').ProjectOptions
  */
 module.exports = {
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: isDev ? process.env.VUE_APP_API_DEV : process.env.VUE_APP_API_PROD,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/',
-        },
-      },
-    },
-  },
-
   pluginOptions: {
     i18n: {
       locale: 'zh-cmn-Hans',
@@ -43,54 +27,6 @@ module.exports = {
     },
   },
 
-  configureWebpack: {
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-              drop_debugger: true,
-            },
-          },
-        }),
-      ],
-      splitChunks: {
-        cacheGroups: {
-          vue: {
-            name: 'chunk-vue',
-            test: /[\\/]node_modules[\\/]vue[\\/]/,
-            priority: 10,
-            chunks: 'initial',
-          },
-          vuex: {
-            name: 'chunk-vuex',
-            test: /[\\/]node_modules[\\/]vuex[\\/]/,
-            priority: 10,
-            chunks: 'initial',
-          },
-          'vue-router': {
-            name: 'chunk-vue-router',
-            test: /[\\/]node_modules[\\/]vue-router[\\/]/,
-            priority: 10,
-            chunks: 'initial',
-          },
-          swiper: {
-            name: 'chunk-swiper',
-            test: /[\\/]node_modules[\\/]swiper[\\/]/,
-            priority: 10,
-            chunks: 'initial',
-          },
-          axios: {
-            name: 'chunk-axios',
-            test: /[\\/]node_modules[\\/]axios[\\/]/,
-            priority: 10,
-            chunks: 'initial',
-          },
-        },
-      },
-    },
-  },
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
       config.plugin('hard-source-webpack-plugin').use(new HardSourceWebpackPlugin());
