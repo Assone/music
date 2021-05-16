31cdb29d0ca6ab4d3dfb533bc68bb693a7535565
a3onny <a3onny@gmail.com>
2021-05-16 17:45:25 +0800

    移除sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue';
-import * as Sentry from '@sentry/vue';
-import { Integrations } from '@sentry/tracing';
 import 'vite-plugin-svg-icons/register';
 
 import App from './App.vue';
@@ -11,31 +9,6 @@ import './plugins/interceptors';
 
 import './styles/index.scss';
 import i18n from './plugins/i18n';
-import { isProd } from './utils';
 
-const app = createApp(App);
-app.use(router).use(store, key).use(i18n).mount('#app');
+createApp(App).use(router).use(store, key).use(i18n).mount('#app');
 
-if (isProd) {
-  Sentry.init({
-    dsn: 'https://2adfdf38a00d4fdfa69e38298e3ee9a1@o571492.ingest.sentry.io/5719799',
-    integrations: [new Integrations.BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-
-  app.config.errorHandler = (err) => {
-    Sentry.captureException(err);
-  };
-
-  window.addEventListener('error', (err) => {
-    Sentry.captureException(err);
-  });
-
-  window.addEventListener('unhandledrejection', (err) => {
-    Sentry.captureException(err);
-  });
-}
